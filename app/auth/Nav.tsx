86471e3fedde1f91e5b07da6a954d4bb51505d98
John-Flavian <flavianomeokwe@gmail.com>
2023-07-05 15:10:39 +0100

    Integrates the login process to display the UserName on the Nav component.

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -1,15 +1,21 @@
 import Link from "next/link";
 import Login from "./Login";
+import { authOptions } from "../../pages/api/auth/[...nextauth]"
+import {getServerSession} from 'next-auth/next'
 
 
 export default async function Nav() {
+   // eslint-disable-next-line
+    const session = await getServerSession(authOptions);
+    console.log(session)
     return (
         <nav className="flex justify-between items-center py-8">
             <Link href={'/'}>
                 <h1 className="font-bold text-lg" >Send It</h1>
             </Link>  
             <ul className="flex items-center gap-6">
-                <Login />
+                {!session?.user && <Login />}
+                {session?.user && <h1>{session.user.name}</h1>}
             </ul>
         </nav>
     )
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,20 +1,20 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from '../../../prisma/client'
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from "../../../prisma/client"
 
-
+const adapter = PrismaAdapter(prisma)
 
 export const authOptions = {
-  // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
-  ]
-  
+	// Configure one or more authentication providers
+	adapter: PrismaAdapter(prisma),
+	secret: process.env.AUTH_SECRET,
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		}),
+	],
 }
 
 export default NextAuth(authOptions)
