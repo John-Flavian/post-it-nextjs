6570199bf242758cdde626b8b7b0ab657e151243
John-Flavian <flavianomeokwe@gmail.com>
2023-07-05 16:30:13 +0100

    feat: adds the User image to the home page.

---
+++ app/auth/Logged.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Image from "next/image";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+
+type User = {
+  image: string;
+};
+
+export default function Logged({ image }: User) {
+  return (
+    <li className="flex gap-8 items-center">
+      <button
+        onClick={() => signOut()}
+        className="bg-gray-700 text-white text-sm px-6 py-2 rounded-md"
+      >
+        Sign Out
+      </button>
+      <Link href={"/dashboard"}>
+        <Image
+          src={image}
+          width={64}
+          height={64}
+          alt="Profile-Image"
+          className="w14 rounded-full"
+          priority
+        />
+      </Link>
+    </li>
+  );
+}
