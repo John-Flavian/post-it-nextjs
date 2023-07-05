82aaf6ce5300a3b632f6f8699412747c44816e76
John-Flavian <flavianomeokwe@gmail.com>
2023-07-05 12:25:39 +0100

    feat: Adds the Login component to the nav.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
+import Nav from "./auth/Nav"
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto"
+})
 
 export const metadata = {
   title: 'Post It App',
@@ -15,7 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`mx-4 md:mx-48 xl:mx-96 bg-gray-200 ${roboto.variable}`}>
+        <Nav />
+        {children}
+      </body>
     </html>
   )
 }
