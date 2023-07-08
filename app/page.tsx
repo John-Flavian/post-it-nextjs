336603cf50a70d2df7a2aa1ab94a4300d70e61d6
John-Flavian <flavianomeokwe@gmail.com>
2023-07-08 10:52:16 +0100

    feat: adds the getPosts function to the homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,37 @@
 "use client";
+import axios from "axios";
 import AddPost from "../app/components/AddPost";
+import { useQuery } from "@tanstack/react-query";
+import Post from "./components/Posts";
+
+//Fetch all posts
+const allPosts = async () => {
+  const response = await axios.get("/api/getPosts");
+  return response.data;
+};
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  });
+
+  if (error) return error;
+  if (isLoading) return "loading...";
+  console.log(data);
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello main</h1>
       <AddPost />
+      {data?.map((post) => (
+        <Post
+          key={post.id}
+          name={post.user.name}
+          avatar={post.user.image}
+          postTitle={post.title}
+          id={post.id}
+        />
+      ))}
     </main>
   );
 }
---
+++ pages/api/getPosts.js
@@ -0,0 +1,21 @@
+import prisma from "../../prisma/client";
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    try {
+      const result = await prisma.post.findMany({
+        include: {
+          user: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+      res.status(200).json(result);
+    } catch (err) {
+      res
+        .status(403)
+        .json({ err: "Error has occured whilst getting the posts." });
+    }
+  }
+}
