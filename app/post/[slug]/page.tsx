4c361dc322ac50be19220485040d9d52f409c170
John-Flavian <flavianomeokwe@gmail.com>
2023-07-15 18:44:10 +0100

    feat: Displays the comments on the post page.

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -5,6 +5,7 @@ import Post from "@/app/components/Posts";
 import { PostType } from "@/app/types/Post";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import Image from "next/image";
 
 type URL = {
   params: {
@@ -35,6 +36,16 @@ export default function PostDetail(url: URL) {
         comments={data.comment}
       />
       <AddComment id={data.id} />
+      {data?.comment?.map((c) => (
+        <div key={c.id} className="my-6 bg-white p-8 rounded-md">
+          <div className="flex items-center gap-2">
+            <Image width={24} height={24} src={c.user?.image} alt="avatar" />
+            <h3 className="font-bold">{c?.user?.name}</h3>
+            <h2 className="text-sm">{c.createdAt}</h2>
+          </div>
+          <div className="py-4">{c.message}</div>
+        </div>
+      ))}
     </div>
   );
 }
--- app/types/Post.ts
+++ app/types/Post.ts
@@ -21,4 +21,4 @@ export type PostType = {
       name: string;
     };
   }[];
-};
+}[];
